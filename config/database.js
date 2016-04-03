cf4b3fb53bb55dfb886a50aa05cc4bac18b1e98a
iamquang95 <iamquang95@gmail.com>
2016-04-03 15:25:17 +0700

    First deploy

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    remoteUrl : 'mongodb://test1:test1@ds013300.mlab.com:13300/iamquang95-todo-list',
+    localUrl: 'mongodb://test1:test1@ds013300.mlab.com:13300/iamquang95-todo-list'
 };
